5e9225d7bef6859696a157c31e6bf37e2d2e543c
Aphyr <aphyr@aphyr.com>
2022-06-28 12:26:26 -0400

    Allow deterministic replay of edn histories with --history

--- src/jepsen/local_fs.clj
+++ src/jepsen/local_fs.clj
@@ -1,8 +1,10 @@
 (ns jepsen.local-fs
   "Main entry point for local FS testing. Handles CLI arguments, launches
   tests."
-  (:require [clojure [pprint :refer [pprint]]
+  (:require [clojure [edn :as edn]
+                     [pprint :refer [pprint]]
                      [string :as str]]
+            [clojure.java.io :as io]
             [clojure.tools.logging :refer [info warn]]
             [jepsen [checker :as checker]
                     [cli :as cli]
@@ -14,9 +16,12 @@
             [jepsen.local-fs.db [dir :as db.dir]
                                 [lazyfs :as db.lazyfs]]
             [clojure.test.check :as tc]
-            [clojure.test.check [properties :as prop]
+            [clojure.test.check [generators :as tc.gen]
+                                [properties :as prop]
                                 [results :refer [Result]]])
-  (:import (java.net URLEncoder)))
+  (:import (java.net URLEncoder)
+           (java.io PushbackReader)
+           (io.lacuna.bifurcan Set)))
 
 (def dbs
   "A map of DB names to functions that take CLI options and build DBs."
@@ -30,6 +35,41 @@
     (check [this test history opts]
       (-> (checker/check (checker/stats) test history opts)
           (assoc :valid? true)))))
+
+(defrecord TapeGen [ops]
+  gen/Generator
+  (op [this test context]
+      (when-let [op (first ops)]
+        (let [p      (:process op)
+              thread (gen/process->thread context p)
+              free-threads ^Set (:free-threads context)]
+          ; Only emit an op if the process is free to execute it.
+          (if (.contains free-threads thread)
+            [(gen/fill-in-op op context) (TapeGen. (next ops))]
+            [:pending this]))))
+
+  (update [this test context event]
+          this))
+
+(defn history-file->gen
+  "Takes a filename and returns a generator from it."
+  [file]
+  (let [; First, load the critical data we need from disk
+        ops (with-open [file (java.io.PushbackReader.
+                               (io/reader file))]
+              (->> (repeatedly #(edn/read {:eof ::eof} file))
+                   (take-while (complement #{::eof}))
+                   (keep (fn [op]
+                           (when (= :invoke (:type op))
+                             ; This won't perfectly reproduce the concurrency
+                             ; structure of the original history since we have
+                             ; no control over how long operations take, but it
+                             ; might get us kinda close?
+                             (select-keys op [:process :f :value]))))
+                   vec))]
+    ; Now construct a generator which zips through this history, dispatching
+    ; ops only when those processes are free.
+    (TapeGen. ops)))
 
 (defn shell-test
   "Takes CLI options and constructs a test for the shell workload."
@@ -50,7 +90,15 @@
                         :stats      (stats-checker)
                         :exceptions (checker/unhandled-exceptions)
                         :workload   (:checker workload)})
-            :client         (:client workload)})))
+            :client         (:client workload)
+            :generator (->> (if-let [file (:history opts)]
+                              (history-file->gen file)
+                              (->> workload
+                                   :test-check-gen
+                                   tc.gen/sample-seq
+                                   (mapcat identity)
+                                   (gen/time-limit (:time-limit opts))))
+                            gen/clients)})))
 
 (defn test-check-gen
   "Takes CLI options and returns a test.check generator."
@@ -67,6 +115,12 @@
 
    [nil "--dir DIR" "What directory should we use for our mount point?"
     :default "data"]
+
+   [nil "--history FILE" "Run a history taken directly from an EDN file. This
+    allows you to shrink a history by hand, in case test.check's shrinking
+    finds a very long example and gets stuck. Just copy history.edn from a
+    test, edit it with a text editor, save it, and run it with `lein run
+    test --history custom.edn`. This won't shrink at all, sadly."]
 
    ["-v" "--version VERSION" "A version string, passed to the DB. For lazyfs, this controls the git commit we check out."]])
 
@@ -112,5 +166,7 @@
   [& args]
   (cli/run!
     (merge (cli/serve-cmd)
+           (cli/single-test-cmd {:test-fn shell-test
+                                 :opt-spec cli-opts})
            quickcheck-cmd)
     args))
